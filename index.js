fd6330937cdbcddc088884d0af56fce8b72f6b7e
Sara Smith <sarasmith887@gmail.com>
2017-11-11 04:24:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 let currentUser = "Sara";
-lconst welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+lconst welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 
-let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser} !`;
 
 let shortGreeting = "Welcome, ";
