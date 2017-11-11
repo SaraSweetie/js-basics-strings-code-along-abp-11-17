8d169b7455b90d49d0728cedfc5296221c3c227b
Sara Smith <sarasmith887@gmail.com>
2017-11-11 04:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let currentUser = "Sara";
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+
+let shortGreeting = "welcome";
